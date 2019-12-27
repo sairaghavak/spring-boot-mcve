983bc5d8eb7c3eb87dfced8a84128528220eb43a
Sai Raghava Katepally <ksairaghava1@gmail.com>
2019-12-27 23:15:12 +0530

    Added @Transactional annotation just for future reference, in this use-case it is optional

--- spring-boot-intro-with-h2-in-mem/src/main/java/com/sairaghava/students/StudentRepository.java
+++ spring-boot-intro-with-h2-in-mem/src/main/java/com/sairaghava/students/StudentRepository.java
@@ -1,7 +1,13 @@
 package com.sairaghava.students;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /*- @Repository // this is not required as @EnableJPARepositories in your main class scans for all reposiotries i.e., all types that extend from Spring Data's JPA repository hierarchy */
+
+@Transactional(propagation = Propagation.MANDATORY)
+// optional in this use-case but just adding to be aware that it must be used
+// most of the times
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 }
